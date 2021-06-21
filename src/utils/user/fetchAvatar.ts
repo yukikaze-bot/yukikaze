669a5c11bd02feb11ca9d8d61eb0d33a21257e5a
1chiSensei <mail@1chi.tk>
2021-06-21 22:29:04 +0800

    fix(i18n): fix lang bug

--- src/utils/user/fetchAvatar.ts
+++ src/utils/user/fetchAvatar.ts
@@ -2,7 +2,7 @@ import type { User, ImageSize } from 'discord.js';
 import { Image, loadImage } from 'canvas';
 
 export const fetchAvatar = async (user: User, size: ImageSize = 512): Promise<Image> => {
-	const url = user.avatar ? user.avatarURL({ format: 'png', size })! : user.defaultAvatarURL;
+	const url = user.displayAvatarURL({ format: 'png', size });
 
 	return loadImage(url);
 };
