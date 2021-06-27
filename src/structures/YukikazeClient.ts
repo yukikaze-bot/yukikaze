19aeac1616231534fe145e36844d3e9b30e4606a
Yoshida Tomio <mail@1chi.tk>
2021-06-27 14:03:08 +0800

    Update YukikazeClient.ts

--- src/structures/YukikazeClient.ts
+++ src/structures/YukikazeClient.ts
@@ -135,7 +135,9 @@ export class YukikazeClient extends SapphireClient {
 					commands: () => this.stores.get('commands').size,
 					channels: () => this.channels.cache.size
 				}
-			}
+			},
+			playground: true,
+			introspection: true
 		});
 
 		await server.listen({ port: 4000 });
